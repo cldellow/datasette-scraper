fa13e85d0a50e29bd77f4da80eb8323a1e94259b
Colin Dellow <cldellow@gmail.com>
2022-12-30 20:48:24 -0500

    make pkeys less obnoxious in stats tables

--- datasette_scraper/__init__.py
+++ datasette_scraper/__init__.py
@@ -44,6 +44,10 @@ def prepare_jinja2_environment(env, datasette):
                 path = 'table-dss_job.html'
             elif path.startswith('table-') and path.endswith('-dss_host_rate_limit.html'):
                 path = 'table-dss_host_rate_limit.html'
+            elif path.startswith('table-') and path.endswith('-dss_job_stats.html'):
+                path = 'table-dss_job_stats.html'
+            elif path.startswith('table-') and path.endswith('-dss_extract_stats.html'):
+                path = 'table-dss_extract_stats.html'
             elif path.startswith('row-') and path.endswith('-dss_crawl.html'):
                 path = 'row-dss_crawl.html'
             elif path.startswith('row-') and path.endswith('-dss_host_rate_limit.html'):
